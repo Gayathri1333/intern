bc811f57dc6b6022e9180e9c5880a250ce40a81e
Gayathri1333 <20bq1a05h2@vvit.net>
2023-05-16 10:56:24 +0530

    update

--- treasurehunt/client/src/AdminGameEvent.js
+++ treasurehunt/client/src/AdminGameEvent.js
@@ -8,7 +8,8 @@ function AdminGameEvent()
       <div className="container text-center my-5 pt-5">
         <br/>
           <div className="row d-flex justify-content-around my-5 pt-5">
-            
+            <p>There are 3 images and go to any of the image based on your clue and hover on the one which you feel correct for the clue and on that image you will getting the next clue </p>
+            <p>Here there are minimum 2 deadends if there is any deadend then start from other clue or the same clues other relevant image if present in the image</p>
              <AdminGameCard/>
           </div>
       </div>
