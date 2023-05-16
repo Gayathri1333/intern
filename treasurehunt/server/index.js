863d86c205cb88f92026939ea5285f34c43ea5a4
Gayathri1333 <20bq1a05h2@vvit.net>
2023-05-16 11:27:21 +0530

    commit

--- treasurehunt/server/index.js
+++ treasurehunt/server/index.js
@@ -4,8 +4,10 @@ const mysql=require('mysql');
 const cors = require('cors');
 
 app.use(cors());
-app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 const db = mysql.createConnection({
     user:"root",
     host:"localhost",
